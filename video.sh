370ad09d31a0d02634d414e509a2475e31091f38
djungelorm <djungelorm@users.noreply.github.com>
2016-08-05 10:04:16 -0700

    Improve video generation

--- video.sh
+++ video.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm samples/*.png
-for f in samples/*.txt; do convert -size 800x600 xc:black -font "FreeMono" -pointsize 60 -fill white -draw @$f $f.png; echo $f; done
-ffmpeg -y -framerate 1000 -i samples/%08d.txt.png output/samples.mkv
+for f in samples/*.txt; do convert -size 800x600 xc:black -font "FreeMono" -pointsize 50 -fill white -draw @$f $f.png; echo $f; done
+ffmpeg -y -framerate 10000 -i samples/%08d.txt.png output/samples.mkv
 cp samples/data.csv ./output/
