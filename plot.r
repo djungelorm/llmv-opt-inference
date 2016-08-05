b0897faa31ca0f28b07a079145ac5a53cfc97ea1
djungelorm <djungelorm@users.noreply.github.com>
2016-08-05 10:03:50 -0700

    Fix plotting script

--- plot.r
+++ plot.r
@@ -3,4 +3,4 @@
 data <- read.csv('output/data.csv')
 par(bg='black')
 par(mar=c(5,5,1,1))
-plot(data$cycles, type='l', xlab='Samples', ylab='Cycles', cex.axis=2, cex.lab=2, lwd=4, col='white', col.lab='white', col.main='white', col.axis='white', fg='white')
+plot(data$latency, type='l', xlab='Samples', ylab='Cycles', cex.axis=2, cex.lab=2, lwd=4, col='white', col.lab='white', col.main='white', col.axis='white', fg='white')
